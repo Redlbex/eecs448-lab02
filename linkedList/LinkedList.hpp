819ebefcc05fc25e896c2840ee600f06774b9874
Adam Khan <akhan5280@gmail.com>
2018-08-29 10:26:42 -0500

    Finished size() method

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -29,10 +29,7 @@ bool LinkedList<T>::isEmpty() const
 template <typename T>
 int LinkedList<T>::size() const
 {
-	/** TODO
-		Fix this method
-	*/
- 	return(0);
+	return(m_size);
 }
 
 template <typename T>
@@ -115,6 +112,7 @@ bool LinkedList<T>::removeBack()
 	}
 	delete lastNode;
 	secondintoLast->setNext(nullptr);
+	m_size--;
 	isRemoved = true;
 	return(isRemoved);
 }
